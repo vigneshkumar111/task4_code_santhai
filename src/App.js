8424eb39ae1fbd08eeccb9826d1b01436c8aae1a
vigneshkumar111 <145093081+vigneshkumar111@users.noreply.github.com>
2025-06-29 21:45:44 +0530

    Update App.js with sum calculator changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{ useRef , useState } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const inputRef1 = useRef(null);
+  const inputRef2 = useRef(null);
+  const [sum , setSum] = useState(null);
 
+  const handlecalculate = () => {
+    const value1 = parseFloat(inputRef1.current.value) || 0 ;
+    const value2 = parseFloat(inputRef2.current.value) || 0 ;
+    setSum (value1 + value2);
+  };
+
+
+
+return(
+  <div style = { {padding: '20px' ,fontFamily: 'arial'  }}>
+    <h2> Sum calculator</h2>
+    <input type = 'number' ref={inputRef1} placeholder='Enter a First number'></input>
+    <br/><br/>
+    <input type='number' ref={inputRef2} placeholder='Enter a Seconnd number'></input>
+    <br/><br/>
+    <button onClick={handlecalculate}>Click to sum</button>
+    {sum != null && <p> The sum is:{sum}</p>}
+  </div>
+  );
+} 
 export default App;
